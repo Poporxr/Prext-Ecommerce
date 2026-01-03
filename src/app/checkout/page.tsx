b55fba104edde783fd107ceba81083f169d0219a
Poporxr <poporxr@gmail.com>
2026-01-03 08:17:03 +0100

    commented checkout page for now

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,17 +1,17 @@
-const Payment = () => {
+{/*const Payment = () => {
   return (
     <div className="order-summary w-[40%] p-1.5 bg-[#c4c2c2] rounded-[50px] shadow-2xl mt-10 flex h-[400px]">
         <div className="w-[100%] h-[100%] bg-[#eae8e8] rounded-[45px] p-5 flex flex-col  shadow-2xl">
           <h2 className="font-bold text-lg mb-7 text-center">Payment Sumary</h2>
           <div className="flex font-semibold justify-between mb-3">
             <span>Subtotal</span>
-            <span className="font-bold">{/*formatMoney({priceCents: totals.subtotalCents})*/}</span>
+            <span className="font-bold">{/*formatMoney({priceCents: totals.subtotalCents})}</span>
           </div>
           <div className="flex font-semibold justify-between mb-3">
             <span>Shipping</span>
             <span className="font-bold">{/*
               formatMoney({priceCents : totals.shippingCents})
-              */}</span>
+              }</span>
           </div>
           <div className="flex font-semibold justify-between mb-3">
             <span>Discount(-20%)</span>
@@ -19,14 +19,14 @@ const Payment = () => {
           </div>
           <div className="flex font-semibold justify-between mb-10">
             <span>Delivery Fee</span>
-            <span className="font-bold">{/*formatMoney({priceCents : totals.deliveryFeeCents})*/}</span>
+            <span className="font-bold">{/*formatMoney({priceCents : totals.deliveryFeeCents})}</span>
           </div>
           <hr />
           <div className="flex justify-between font-bold text-lg mt-4">
             <span>Total</span>
             <span>{/*
               formatMoney({priceCents : totals.totalCents})
-             */ }</span>
+              }</span>
           </div>
           <button className="bg-[#000] text-white py-2 px-4 rounded-[20px] mt-[10%] cursor-pointer">
             Pay and Checkout
@@ -35,4 +35,4 @@ const Payment = () => {
       </div> 
             );
 }
-export default Payment;
+export default Payment; */}
